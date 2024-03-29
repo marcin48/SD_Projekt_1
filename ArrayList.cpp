751464249449b0a8ace41448a04aad63ce88e1ae
nowak-tymon <164738134+nowak-tymon@users.noreply.github.com>
2024-03-29 12:22:55 +0100

    Update ArrayList.cpp

--- ArrayList.cpp
+++ ArrayList.cpp
@@ -5,36 +5,36 @@ using namespace std;
 ArrayList::ArrayList() {
     size = 1;
     arr = new int[size];
-    end = size - 1;
+    end = 0;
 }
 ArrayList::~ArrayList() {
-    delete[] arr;
+    delete[] arr;                              // Destruktor usuwa tablice
 }
 
 void ArrayList::insert_front(int data) {
     if (end == size) {
-        int* temp = new int[2 * size];
+        int* temp = new int[2 * size];         // jezeli tablica jest pelna, tworzy tymczasowa dwa razy wieksza tablice
 
-        for (int i = 0; i < end; i++) {
-            temp[i + 1] = arr[i];
+        for (int i = 0; i < end; i++) {        // nastepnie wpisuje wszystkie elementy ze starej tablicy do nowej
+            temp[i + 1] = arr[i];              // z indeksem przesunietym o 1 w lewo 
         }
 
         delete[] arr;
         size *= 2;
-        arr = temp;
+        arr = temp;                            // kasuje stara tablice, zwieksza size razy 2, i zamienia tablice tymczasowa w domyslna
     }
-    else {
+    else {                                     // przesuwa od konca wszystkie elementy o jeden w lewo 
         for (int i = end; i > 0; i--) {
             arr[i] = arr[i - 1];
         }
     }
 
     arr[0] = data;
-    end++;
+    end++;                                     // wstawia dane na poczatek tablicy i zwieksza wartosc end 
 }
 
 void ArrayList::insert(int data, int index) {
-    if (index < end) {
+    if (index <= end) {
         if (end == size) {
             int* temp = new int[2 * size];
 
@@ -78,11 +78,26 @@ void ArrayList::insert_back(int data) {
     end++;
 }
 
+void ArrayList::remove_front() {
+    if (end > 0) {
+        for (int i = 0; i < end - 1; i++) {
+            arr[i] = arr[i + 1];
+        }
+        end--;
+    }
+}
+
 void ArrayList::remove(int index) {
     if (index < end) {
         for (int i = index; i < end - 1; i++) {
             arr[i] = arr[i + 1];
         }
+        end--;
+    }
+}
+
+void ArrayList::remove_end() {
+    if (end > 0) {
         end--;
     }
 }
@@ -101,6 +116,10 @@ void  ArrayList::print() {
         cout << arr[i] << " ";
     }
     cout << endl;
+}
+
+int ArrayList::get_size() {
+    return size;
 }
 
 
