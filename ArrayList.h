8c287d32659bba8f8dee233fed4df85da898a2d9
nowak-tymon <164738134+nowak-tymon@users.noreply.github.com>
2024-03-29 12:23:31 +0100

    Update ArrayList.h

--- ArrayList.h
+++ ArrayList.h
@@ -1,24 +1,27 @@
 #pragma once
 #include <iostream>
 using namespace std;
+
 class ArrayList 
 {
 private:
-    int* arr;
-    int size;
-    int end;
+    int* arr;  
+    int size;   
+    int end;    
 
 public:
     ArrayList();
     ~ArrayList();
 
-    void insert_front(int data);
-    void insert(int index, int data);
-    void insert_back(int data);
+    void insert_front(int data);        
+    void insert(int data, int index);  
+    void insert_back(int data);        
+    void remove_front();
     void remove(int index);
-    int search(int data);
-    void print();
-    // test czy dziala pushowanie kodu 
-    
+    void remove_end();
+    int search(int data);              
+    void print();                       
+    int get_size();
+
 };
 
