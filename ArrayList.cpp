2856f698f76e9cf23ddffaf4d3618066a44a42be
nowak-tymon <164738134+nowak-tymon@users.noreply.github.com>
2024-04-02 17:38:27 +0200

    Update ArrayList.cpp

--- ArrayList.cpp
+++ ArrayList.cpp
@@ -2,128 +2,141 @@
 #include "ArrayList.h"
 using namespace std;
 
-ArrayList::ArrayList() { 
-    size = 1;
-    arr = new int[size];
-    end = 0;
-}
-ArrayList::~ArrayList() {
-    delete[] arr;                              // Destruktor usuwa tablice
+
+ArrayList::ArrayList() {
+    size = 1; 
+    arr = new int[size]; 
+    end = 0; 
 }
 
+
+ArrayList::~ArrayList() {
+    delete[] arr; 
+}
+
+// Wstawia element na początek tablicy
 void ArrayList::insert_front(int data) {
-    if (end == size) {
-        int* temp = new int[2 * size];         // jezeli tablica jest pelna, tworzy tymczasowa dwa razy wieksza tablice
-                                                
-        for (int i = 0; i < end; i++) {        // nastepnie wpisuje wszystkie elementy ze starej tablicy do nowej
-            temp[i + 1] = arr[i];              // z indeksem przesunietym o 1 w lewo 
+    if (end == size) { 
+        int* temp = new int[2 * size]; // Tworzy tymczasową tablicę o dwukrotnie większym rozmiarze
+
+        for (int i = 0; i < end; i++) { // Przenosi wszystkie elementy ze starej tablicy do nowej
+            temp[i + 1] = arr[i]; // Z indeksem przesuniętym o 1 w prawo 
         }
 
-        delete[] arr;                          
-        size *= 2;
-        arr = temp;                            // kasuje stara tablice, zwieksza size razy 2, i zamienia tablice tymczasowa w domyslna
+        delete[] arr; 
+        size *= 2; 
+        arr = temp; 
     }
-    else {                                     // przesuwa od konca wszystkie elementy o jeden w lewo 
+    else { // Przesuwa wszystkie elementy o jeden w prawo 
         for (int i = end; i > 0; i--) {
             arr[i] = arr[i - 1];
         }
     }
 
-    arr[0] = data;                             
-    end++;                                     // wstawia dane na poczatek tablicy i zwieksza wartosc end 
+    arr[0] = data; 
+    end++; 
 }
 
+// Wstawia element na określonej pozycji w tablicy
 void ArrayList::insert(int data, int index) {
-    if (index <= end) {
-        if (end == size) {
-            int* temp = new int[2 * size];
+    if (index <= end) { 
+        if (end == size) { 
+            int* temp = new int[2 * size]; // Tworzy tymczasową tablicę o dwukrotnie większym rozmiarze
 
-            for (int i = 0; i < index; i++) {
+            for (int i = 0; i < index; i++) { // Przenosi elementy ze starej tablicy do nowej
                 temp[i] = arr[i];
             }
 
-            for (int i = index; i < end; i++) {
-                temp[i + 1] = arr[i];
+            for (int i = index; i < end; i++) { // Przenosi resztę elementów ze starej tablicy do nowej
+                temp[i + 1] = arr[i]; // Z indeksem przesuniętym o 1 w prawo
             }
 
-            delete[] arr;
-            size *= 2;
-            arr = temp;
+            delete[] arr; 
+            size *= 2; 
+            arr = temp; 
         }
-        else {
-            for (int i = end; i > index; i--) {
+        else { 
+            for (int i = end; i > index; i--) { // Przesuwa wszystkie elementy o jeden w prawo od indeksu
                 arr[i] = arr[i - 1];
             }
         }
 
-        arr[index] = data;
-        end++;
+        arr[index] = data; 
+        end++; 
     }
 }
 
+// Wstawia element na koniec tablicy
 void ArrayList::insert_back(int data) {
-    if (end == size) {
-        int* temp = new int[2 * size];
+    if (end == size) { 
+        int* temp = new int[2 * size]; // Tworzy tymczasową tablicę o dwukrotnie większym rozmiarze
 
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < size; i++) { // Przenosi wszystkie elementy ze starej tablicy do nowej
             temp[i] = arr[i];
         }
 
-        delete[] arr;
-        size *= 2;
-        arr = temp;
+        delete[] arr; 
+        size *= 2; 
+        arr = temp; 
     }
 
-    arr[end] = data;
-    end++;
+    arr[end] = data; 
+    end++; 
 }
 
+// Usuwa element z początku tablicy
 void ArrayList::remove_front() {
-    if (end > 0) {
-        for (int i = 0; i < end - 1; i++) {
+    if (end > 0) { 
+        for (int i = 0; i < end - 1; i++) { // Przesuwa wszystkie elementy o jeden w lewo
             arr[i] = arr[i + 1];
         }
-        end--;
+        end--; 
     }
 }
 
+// Usuwa element z określonej pozycji w tablicy
 void ArrayList::remove(int index) {
-    if (index < end) {
-        for (int i = index; i < end - 1; i++) {
+    if (index < end) { 
+        for (int i = index; i < end - 1; i++) { // Przesuwa wszystkie elementy o jeden w lewo od indeksu
             arr[i] = arr[i + 1];
         }
-        end--;
+        end--; 
     }
 }
 
+// Usuwa element z końca tablicy
 void ArrayList::remove_back() {
-    if (end > 0) {
-        end--;
+    if (end > 0) { 
+        end--; 
     }
 }
 
+// Szuka elementu w tablicy
 int ArrayList::search(int data) {
-    for (int i = 0; i < end; i++) {
-        if (arr[i] == data) {
-            return i;
+    for (int i = 0; i < end; i++) { // Przeszukuje tablicę
+        if (arr[i] == data) { // Jeżeli znajdzie szukany element zwraca go
+            return i; 
         }
     }
-    return -1;
+    return -1; 
 }
 
+// Wyświetla wszystkie elementy tablicy
 void  ArrayList::print() {
-    for (int i = 0; i < end; i++) {
-        cout << arr[i] << " ";
+    for (int i = 0; i < end; i++) { 
+        cout << arr[i] << " "; 
     }
-    cout << endl;
+    cout << endl; 
 }
+
 
 void ArrayList::load_file() {};
 
-
+// Zwraca rozmiar tablicy
 int ArrayList::get_size() {
     return size;
 }
+
 
 
 
